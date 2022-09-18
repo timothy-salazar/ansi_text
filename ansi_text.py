1c953ed2c84003616111e4d39685f1eeba79a9d5
timothy-salazar <timothy.salazar@colorado.edu>
2022-09-17 23:48:48 -0600

    working on tests

--- ansi_text.py
+++ ansi_text.py
@@ -2,45 +2,101 @@
 Timothy Salazar
 2022-09-16
 """
-from collections.abc import Sequence
+from collections.abc import MutableSequence
 import re
 
 
-class AnsiText(Sequence):
-    """ Ansi text thing
+class AnsiText(MutableSequence):
+    """ Reads in text that has had formatting applied using ANSI escape
+    sequences (colored foreground, colored background, bold, underline, etc.).
+    The text can then be accessed and manipulated in a number of ways.
+        - The characters of the unformatted text can be 
     """
     def __init__(self):
         """ things and stuff
         """
-        self.text = ''
+        self.plaintext = []
         self.format_str = ''
         self.format_dict = dict()
-        self.raw = ''
+        # self.raw = ''
+        self.regex = ''
+        self.regex_stuff()
+        self.slice_dict = dict()
 
     def __getitem__(self, index):
-        return self.text[index]
+        return self.plaintext[index]
+
+    def __setitem__(self, index, value):
+        # temp = list(self.plaintext)
+        # temp[index] = value
+        # self.plaintext = ''.join(temp)
+        self.plaintext = self.plaintext[:index] + value + self.plaintext[index+1:]
+
+    def __delitem__(self, index):
+        # del self.plaintext[index]
+        pass
+
+    def insert(self, index, value):
+        # self.plaintext.insert(index, value)
+        pass
 
     def __len__(self):
-        return len(self.text)
-    
+        return len(self.plaintext)
+
     def __str__(self):
-        return self.raw
+        # return ''.join(self.format_dict.values())
+        # return self.raw
+        return self.format_str.format(*[
+            ''.join(self.plaintext[v])
+            for v in self.format_dict.values()])
 
     def read(self, raw):
+        """ 
         """
+        # self.raw = raw
+        matches = [i for i in re.finditer(self.regex, raw)]
+        # self.plaintext = ''.join([m.group('text') for m in matches])
+
+        get_group = lambda m, name: m.group(name) if m.group(name) else ''
+        self.format_str = ''.join(
+            [get_group(m, 'fmt') + f'{{{v}}}' + get_group(m, 'end')
+             for v, m in enumerate(matches)])
+        # self.format_dict = {v:m.group('text') for v, m in enumerate(matches)}
+        # self.format_dict = {v:m.group('text') for v, m in enumerate(matches)}
+
+        # self.slice_dict = {v:slice(m.start('text'), m.end('text')) for v,m in enumerate(matches)}
+
+        s = ''
+        for v, m in enumerate(matches):
+            # t = list(m.group('text'))
+            t = m.group('text')
+            self.format_dict[v] = slice(len(s), len(s)+len(t))
+            s += t
+        self.plaintext = s
+
+    def regex_stuff(self):
+        """ Builds the regular expression we'll use to process our text.
+        I put it down here to keep things tidy.
         """
-        get_char_expression = r'''
-        \x1b\[          # This begins the escape sequence
-        (?:[0-9]{1,3};)+? # semicolon-separated parameters
-        [0-9]{1,3}m     # final parameter
-        (?P<char>.+?)     # the printable character
-        (?=\x1b\[0m)        # resets everything back
+
+        ctr_seq = r'''
+            \x1b\[          # This marks the start of a control sequence
+            ([0-9]{1,3};)*? # semicolon-separated parameters
+            [0-9]{1,3}m     # final parameter
         '''
-        r = re.compile(get_char_expression, re.VERBOSE)
-        r.findall(raw)
-        self.raw = raw
-        self.text = ''.join(r.findall(raw))
+        end = fr'''
+          (?P<end>      # group name
+              {ctr_seq} # matches a control sequence
+           )*\Z         # This makes it so we'll always match the end, even if
+        '''             #   the string doesn't end with a control sequence
 
-
-    # def __str__(self):
-    #     print(self.text)
+        tst = fr'''(?mxs)       # Sets MULTILINE, VERBOSE, and DOTALL flags
+            (
+                (?P<fmt>({ctr_seq})+)  # matches one or more control sequences
+                |(?=[^\x1b])?   # This lets us skip ahead when the string 
+            )                   # doesn't begin with a control sequence
+            (?P<text>.+?)       # The plaintext
+            ({end}              # Either a control sequence ending the string or None
+            |(?={ctr_seq}))     # Looks ahead for control sequences not 
+        '''                     # ending the string
+        self.regex = tst
--- test_ansi.py
+++ test_ansi.py
@@ -1,61 +1,207 @@
+import re
+import textwrap
 from ansi_text import AnsiText
 
-def test_basic():
-    "Most basic test case"
-    text = 'some basic text'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert str(AnsiText) == text
-    assert ansi_text.format_str == ''
-    assert ansi_text.format_dict == dict()
 
-def test_one_color_foreground():
-    "Tests string with one type of formatting applied throughout"
-    text = '\x1b[38;5;12mstuff\x1b[0m'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert str(AnsiText) == text
-    assert ansi_text.format_str == '\x1b[38;5;12m{0}\x1b[0m'
-    assert ansi_text.format_dict == {0:'stuff'}
-    
-def test_two_color_foreground():
-    """ tests string with one type of formatting applied to the first half,
-    and a second type of formatting applied to the second half
+CLEAR = '\x1b[0m'
+
+class TestBasic:
+    """ Basic test cases where we provide AnsiText with input
+    and examine the output
     """
-    text = '\x1b[38;5;12mstuff\x1b[0m\x1b[38;5;9mthings\x1b[0m'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert str(AnsiText) == text
-    assert ansi_text.format_str == '\x1b[38;5;12m{0}\x1b[0m\x1b[38;5;9m{1}\x1b[0m'
-    assert ansi_text.format_dict == {0:'stuff', 1:'things'}
+    def test_basic(self):
+        "Most basic test case"
+        raw = 'some basic text'
+        ansi_text = AnsiText()
+        ansi_text.read(raw)
+        assert str(ansi_text) == raw
+        assert ansi_text.plaintext == raw
+        assert ansi_text.format_str == '{0}'
+        # assert ansi_text.format_dict == {0:raw}
 
+    def test_one_color_foreground(self):
+        "Tests string with one type of formatting applied throughout"
+        text = '\x1b[38;5;12mstuff\x1b[0m'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert str(ansi_text) == text
+        assert ansi_text.plaintext == 'stuff'
+        assert ansi_text.format_str == '\x1b[38;5;12m{0}\x1b[0m'
+        # assert ansi_text.format_dict == {0:'stuff'}
 
-def test_indexing_basic():
-    " tests whether we can index into the string properly"
-    text = 'some basic text'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert ansi_text[:] == text
-    assert ansi_text[0] == 's'
-    assert ansi_text[:6] == 'some b'
-    assert ansi_text[-1] == 't'
+    def test_one_color_no_end(self):
+        "Tests string with one type of formatting, no end"
+        text = '\x1b[38;5;12mstuff'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert str(ansi_text) == text
+        assert ansi_text.plaintext == 'stuff'
+        assert ansi_text.format_str == '\x1b[38;5;12m{0}'
+        # assert ansi_text.format_dict == {0:'stuff'}
 
-def test_indexing_colored_text():
-    " tests whether we can index into a string with basic ansi formatting"
-    text = '\x1b[38;5;12mstuff\x1b[0m'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert ansi_text[:] == 'stuff'
-    assert ansi_text[0] == 's'
-    assert ansi_text[:3] == 'stu'
-    assert ansi_text[-1] == 'f'
+    def test_one_color_partway_through(self):
+        "Tests string with one type of formatting, no end"
+        text = 'things and\x1b[38;5;12mstuff\x1b[0m'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert str(ansi_text) == text
+        assert ansi_text.format_str == '{0}\x1b[38;5;12m{1}\x1b[0m'
+        # assert ansi_text.format_dict == {0:'things and', 1:'stuff'}
 
-def test_indexing_two_color_text():
-    " tests whether we can index into a string with basic ansi formatting"
-    text = '\x1b[38;5;12mstuff\x1b[0m\x1b[38;5;9mthings\x1b[0m'
-    ansi_text = AnsiText()
-    ansi_text.read(text)
-    assert ansi_text[:] == 'stuffthings'
-    assert ansi_text[0] == 's'
-    assert ansi_text[:7] == 'stuffth'
-    assert ansi_text[-2] == 'g'
+    def test_one_color_partway_through_no_end(self):
+        "Tests string with one type of formatting, no end"
+        text = 'things and\x1b[38;5;12mstuff'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert str(ansi_text) == text
+        assert ansi_text.plaintext == 'things andstuff'
+        assert ansi_text.format_str == '{0}\x1b[38;5;12m{1}'
+        # assert ansi_text.format_dict == {0:'things and', 1:'stuff'}
+        
+    def test_two_color_foreground(self):
+        """ tests string with one type of formatting applied to the first half,
+        and a second type of formatting applied to the second half
+        """
+        text = '\x1b[38;5;12mstuff\x1b[0m\x1b[38;5;9mthings\x1b[0m'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert str(ansi_text) == text
+        assert ansi_text.plaintext == 'stuffthings'
+        assert ansi_text.format_str == '\x1b[38;5;12m{0}\x1b[0m\x1b[38;5;9m{1}\x1b[0m'
+        # assert ansi_text.format_dict == {0:'stuff', 1:'things'}
+
+class TestIndexing:
+    """ Testing whether AnsiText indexing works as expected
+    """
+    def test_indexing_basic(self):
+        " tests whether we can index into the string properly"
+        text = 'some basic text'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert ansi_text[:] == 'some basic text'
+        assert ansi_text[0] == 's'
+        assert ansi_text[:6] == 'some b'
+        assert ansi_text[-1] == 't'
+
+    def test_indexing_colored_text(self):
+        " tests whether we can index into a string with basic ansi formatting"
+        text = '\x1b[38;5;12mstuff\x1b[0m'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert ansi_text[:] == 'stuff'
+        assert ansi_text[0] == 's'
+        assert ansi_text[:3] == 'stu'
+        assert ansi_text[-1] == 'f'
+
+    def test_indexing_two_color_text(self):
+        " tests whether we can index into a string with basic ansi formatting"
+        text = '\x1b[38;5;12mstuff\x1b[0m\x1b[38;5;9mthings\x1b[0m'
+        ansi_text = AnsiText()
+        ansi_text.read(text)
+        assert ansi_text[:] == 'stuffthings'
+        assert ansi_text[0] == 's'
+        assert ansi_text[:7] == 'stuffth'
+        assert ansi_text[-2] == 'g'
+
+class TestMutable:
+    def test_overwrite(self):
+        pass
+
+########################
+# Testing AnsiText regex
+########################
+class TestRegex:
+    """ Tests whether the AnsiText regex functions as expected
+    """
+    def test_regex_basic(self):
+        """ text with no formatting """
+        text = 'some basic text'
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == [None]
+        assert [i.group('text') for i in m] == [text]
+        assert [i.group('end') for i in m] == [None]
+        assert len(m) == 1
+        assert m[0].span() == (0, len(text))
+
+    def test_regex_basic_multiline(self):
+        """ multiline text with no formatting """
+        text = 'some basic text\nwith newlines and\nstuff!'
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == [None]
+        assert [i.group('text') for i in m] == [text]
+        assert [i.group('end') for i in m] == [None]
+        assert len(m) == 1
+        assert m[0].span() == (0, len(text))
+
+    def test_regex_simple_color(self):
+        """ text with minimal formatting """
+        text = '\x1b[38;5;12mstuff\x1b[0m'
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == ['\x1b[38;5;12m']
+        assert [i.group('text') for i in m] == ['stuff']
+        assert [i.group('end') for i in m] == [CLEAR]
+        assert len(m) == 1
+        assert m[0].span() == (0, len(text))
+
+    def test_regex_color_no_stop(self):
+        """ text with a formatting start, but no clear statement"""
+        text = '\x1b[38;5;12mstuff'
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == ['\x1b[38;5;12m']
+        assert [i.group('text') for i in m] == ['stuff']
+        assert [i.group('end') for i in m]  == [None]
+        assert len(m) == 1
+        assert m[0].span() == (0, len(text))
+
+    def test_regex_multicolor_no_stops(self):
+        " text where the formatting changes without any clear statements"
+        text = '\x1b[38;5;12mstuff\x1b[38;5;9mthings\x1b[38;5;14mdetritus'
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == [
+            '\x1b[38;5;12m',
+            '\x1b[38;5;9m',
+            '\x1b[38;5;14m']
+        assert [i.group('text') for i in m] == [
+            'stuff',
+            'things',
+            'detritus']
+        assert [i.group('end') for i in m] == [None, None, None]
+        assert len(m) == 3
+        assert m[0].span() == (0, 15)
+
+    def test_regex_color_multiline(self):
+        """ multiline text with minimal formatting"""
+        text = textwrap.dedent('''\
+            \x1b[38;5;12mstuff
+            and things
+            and junk\x1b[0m''')
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == ['\x1b[38;5;12m']
+        assert [i.group('text') for i in m] == ['stuff\nand things\nand junk']
+        assert [i.group('end') for i in m] == [CLEAR]
+        assert len(m) == 1
+        assert m[0].span() == (0, len(text))
+
+    def test_regex_multicolor_multiline(self):
+        """ multiline text which changes formatting """
+        text = textwrap.dedent('''\
+            \x1b[38;5;12mstuff
+            and \x1b[0m\x1b[38;5;9mthings
+            and junk\x1b[0m''')
+        ansi_text = AnsiText()
+        m = [i for i in re.finditer(ansi_text.regex, text)]
+        assert [i.group('fmt') for i in m] == [
+            '\x1b[38;5;12m',
+            '\x1b[0m\x1b[38;5;9m']
+        assert [i.group('text') for i in m] == [
+            'stuff\nand ',
+            'things\nand junk']
+        assert [i.group('end') for i in m] == [None, CLEAR]
+        assert len(m) == 2
+        assert m[0].span() == (0, 20)
